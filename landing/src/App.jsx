21ef93b049c0570a1cbab9e18f3ead2e4d26d9bc
Parsanov <romaparsanov@gmail.com>
2024-11-11 22:29:08 +0200

    asdasd

--- landing/src/App.jsx
+++ landing/src/App.jsx
@@ -1,4 +1,5 @@
 import BotLink from "./Componets/BotLink/BotLink";
+import Competitors from "./Componets/Competitors/Competitors";
 import Crew from "./Componets/Crew/Crew";
 import ForWho from "./Componets/ForWho/ForWho";
 import NavBar from "./Componets/NavBar/NavBar"
@@ -19,6 +20,7 @@ function App() {
             <Crew/>
             <Technology/>
             <ForWho/>
+            <Competitors/>
         </BrowserRouter>
     </>
   )
---
+++ landing/src/Componets/Competitors/Competitors.jsx
@@ -0,0 +1,27 @@
+import Time from "../../assets/CompetionImages/Time.jpg"
+
+
+
+const Competitors = () => {
+    return ( 
+        <div class="main-container">
+        <div class="cards-competitors">
+            <div class="card-competitors">
+                <h1 class="title-competitors">Переваги на конкурентами</h1>
+                
+                <div class="image-competitors">
+                    <img src="path-to-your-time-image.jpg" alt="Time comparison"/>
+                </div>
+
+                <div class="description-competitors">
+                    <p>
+                      
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+ );
+}
+ 
+export default Competitors;
